58873920f32d6e40f11c47fc2f8fa1267f2ef6d9
rajsoni7858 <rajsoni7858@gmail.com>
2024-03-05 16:26:01 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React"),
-    React.createElement("h2", {}, "By Raj Soni"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "By React js learner"),
-    React.createElement("h2", {}, "im h2 tag"),
-  ]),
-]);
-console.log(parent);
+//JSX (transpiled before it reaches  the JS)- PARCEL - Babel
 
-// const heading = React.createElement(
-//   "h1",
-//,   { id: "heading", className: "avd" },
-//   "Hello world from react!"
-// );
+//React Element
+const Title = () => (
+  <h1 id="heading" className="head" tabIndex="1">
+    Namaste React using JSX
+  </h1>
+);
+
+//React Functional Component
+//Component Conmposition
+const HeadingComponent = () => (
+  <div className="container">
+    {123151}
+    <Title></Title>
+    <h1>Namaste React Functional Component</h1>
+  </div>
+);
+
+//
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
